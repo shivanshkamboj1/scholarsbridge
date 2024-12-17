bb7a0f0760f61e3e08ddb9a0ba2c1a0bea310bfd
shivansh kamboj <kambojshivansh2004@gmail.com>
2024-12-17 17:52:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,6 +25,8 @@ app.use(
 	cors({
 		origin:process.env.NODE_APP_BASE_URL,
 		credentials:true,
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
+    		allowedHeaders: ["Content-Type", "Authorization"],
 	})
 )
 
