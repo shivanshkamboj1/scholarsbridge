21c9d94a74f611be9cc30c44eee2103a27d4a7f6
shivansh kamboj <kambojshivansh2004@gmail.com>
2024-12-17 17:56:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:process.env.NODE_APP_BASE_URL,
+		origin:https://scholarsbridge.onrender.com,
 		credentials:true,
 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
     		allowedHeaders: ["Content-Type", "Authorization"],
