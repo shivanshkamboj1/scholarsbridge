beb1358350c6de2a6868f2b4bd7a80138395f2c9
shivansh kamboj <kambojshivansh2004@gmail.com>
2025-04-13 23:45:25 +0530

    Update razorpay.js

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -5,5 +5,3 @@ exports.instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
-console.log("RAZORPAY_KEY_ID:", process.env.RAZORPAY_KEY_ID);
-console.log("RAZORPAY_KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET);
